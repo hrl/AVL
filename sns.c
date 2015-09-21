54bd4cc94bd0a50775825a2722fe215f80b105e6
hrl <cxghrl@gmail.com>
2015-09-21 09:00:39 +0800

    sns: add people_compar

--- sns.c
+++ sns.c
@@ -2,4 +2,12 @@
 // Created by hrl on 9/21/15.
 //
 
+#include "sns_structs.h"
+#include "sns_functions.h"
 
+int people_compar(const void *a, const void *b){
+    People *A, *B;
+    A = (People*)a;
+    B = (People*)b;
+    return A->id - B->id;
+}
