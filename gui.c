7e0039348a268b4248f9c43a8b4bc3d4eef4bafa
hrl <cxghrl@gmail.com>
2015-09-22 23:38:07 +0800

    gui: init defines

--- gui.c
+++ gui.c
@@ -1,3 +1,16 @@
 //
 // Created by hrl on 9/22/15.
 //
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <cJSON.h>
+#include <gtk/gtk.h>
+#include "sns_functions.h"
+#include "gui_functions.h"
+#include "sns_structs.h"
+#include "gui_structs.h"
+#include "sns_defines.h"
+#include "gui_defines.h"
--- gui_defines.h
+++ gui_defines.h
@@ -5,4 +5,19 @@
 #ifndef AVL_GUI_DEFINES_H
 #define AVL_GUI_DEFINES_H
 
+#define UI_FILE "UI.xml"
+
+const char *version = "0.1";
+const char *program_name = "数据结构课设演示系统";
+const char *comments = "基于AVL树实现集合，基于集合实现SNS基础功能";
+const char *website = "https://github.com/hrl/AVL";
+const char *website_lable = "GitHub";
+const char *author[] = {"Ruilin Huang", NULL};
+
+FILE *file=NULL;
+short updated=0;
+GtkWindow *window=NULL;
+GtkTreeView *treeview=NULL;
+Sns *SNS=NULL;
+
 #endif //AVL_GUI_DEFINES_H
